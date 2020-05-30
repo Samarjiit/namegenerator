164cfe136ceff74f48841445a811f742f466e93f
samarjit mahi <41121431+Samarjiit@users.noreply.github.com>
2020-05-30 12:07:42 +0530

    Updates

---
+++ precache-manifest.ef1e524b8e2ffbc3ff231381fe650116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28595626742ae7b4080175b60d977798",
+    "url": "/namegenerator/index.html"
+  },
+  {
+    "revision": "76feb7e3b3baa5449d96",
+    "url": "/namegenerator/static/css/main.80893fab.chunk.css"
+  },
+  {
+    "revision": "a4646a883fb6043014cb",
+    "url": "/namegenerator/static/js/2.87788b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/namegenerator/static/js/2.87788b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76feb7e3b3baa5449d96",
+    "url": "/namegenerator/static/js/main.fd8aadfc.chunk.js"
+  },
+  {
+    "revision": "d6c1603da109c09e83cb",
+    "url": "/namegenerator/static/js/runtime-main.7fc58bba.js"
+  }
+]);
